84c3ee9597795cf27414c0c97d436d047cf299b4
AnSantos99 <an.santos95@hotmail.com>
2022-01-03 16:32:11 +0000

    First exercise

---
+++ DoubleBuffer2D/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DoubleBuffer2D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           DoubleBuffer2D<char> db = new DoubleBuffer2D<char>(5, 5);
+
+           db[1, 0] = 'C';
+           db[1, 1] = 'x';
+           db[1, 2] = 'a';
+           db[1, 3] = 's';
+           db[1, 4] = 'd';
+
+           db[2, 0] = 'f';
+           db[2, 1] = 'g';
+           db[2, 2] = 'h';
+           db[2, 3] = 'j';
+           db[2, 4] = 'k';
+
+            db.Swap();
+
+            Console.WriteLine(db.ToString());
+
+        }
+    }
+}
